c239f76f9ec8fe8ca6e74e1421745190f54f5324
akash-code-org <akash.workspace@outlook.com>
2026-07-01 00:02:45 +0500

    Pattern

--- abcdTriangleMast.py
+++ abcdTriangleMast.py
@@ -1,7 +1,7 @@
 for i in range(1,4+1):
     for j in range(1,4+1-i):
         print(" ",end=" ")
-    for k in range(65,4+1):
+    for k in range(65,65+i):
         ch = chr(k)
         print(ch,end=" ")
     print()
